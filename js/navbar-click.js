dcbfb83ab739c1c40576bd209086e80b708ee129
Levko Tkach <91378152+LevkoTkach@users.noreply.github.com>
2023-04-24 12:09:38 +0300

    fis burger-class

--- js/navbar-click.js
+++ js/navbar-click.js
@@ -3,17 +3,17 @@
   const navigationsBody = document.querySelector(".navigation-wrapper");
   iconBurger.addEventListener("click", function (e) {
     document.body.classList.toggle("lock");
-    iconBurger.classList.toggle("active");
-    navigationsBody.classList.toggle("active");
+    iconBurger.classList.toggle("active-burger");
+    navigationsBody.classList.toggle("active-burger");
   });
 
   const navLinks = document.querySelectorAll(".navigation__link");
   navLinks.forEach((navLink) => {
     navLink.addEventListener("click", (_) => {
-          if (iconBurger.classList.contains("active")) {
+          if (iconBurger.classList.contains("active-burger")) {
               document.body.classList.remove("lock");
-              iconBurger.classList.remove("active");
-              navigationsBody.classList.remove("active");
+              iconBurger.classList.remove("active-burger");
+              navigationsBody.classList.remove("active-burger");
           }
         }
     );
