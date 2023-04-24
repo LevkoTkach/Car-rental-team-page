9ee7bfd56928f53f87d5054c55a0a2fe7804f08a
Levko Tkach <91378152+LevkoTkach@users.noreply.github.com>
2023-04-24 11:55:34 +0300

    fix bug

--- js/SaleTimeOut.js
+++ js/SaleTimeOut.js
@@ -2,23 +2,23 @@
 (function () {
 
     const salecontainer = document.querySelector('.sale__clock');
-    const endSaleTime = new Date ('Nov 8 2022 00:00:00');
+    const endSaleTime = Date.now() + 200000000;
 
-    function saleTimer () { 
+    function saleTimer() {
         const timeLeft = endSaleTime - new Date();
 
-        const days = Math.floor(timeLeft/ 1000 / 60 / 60 / 24);
-        const hours = Math.floor(timeLeft/ 1000 / 60 / 60) % 24;
-        const minutes = Math.floor(timeLeft/ 1000 / 60) % 60;
+        const days = Math.floor(timeLeft / 1000 / 60 / 60 / 24);
+        const hours = Math.floor(timeLeft / 1000 / 60 / 60) % 24;
+        const minutes = Math.floor(timeLeft / 1000 / 60) % 60;
         salecontainer.innerText = `${days} days ${hours} hours ${minutes} min`;
-        
+
     };
-    function saleFlash () {
+    function saleFlash() {
         salecontainer.innerText = 'Sale end in';
     };
-               
-    saleTimer(); 
+
+    saleTimer();
     setInterval(saleFlash, 2000);
     setInterval(saleTimer, 4000);
- 
+
 })();
